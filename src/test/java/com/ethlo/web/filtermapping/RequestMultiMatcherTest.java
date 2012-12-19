3324da0565cfb5b870272603164232e801260202
Morten <github@ethlo.com>
2012-12-19 07:34:21 +0100

    Cleanup, faster test bootstrap

--- src/test/java/com/ethlo/web/filtermapping/RequestMultiMatcherTest.java
+++ src/test/java/com/ethlo/web/filtermapping/RequestMultiMatcherTest.java
@@ -5,30 +5,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.ethlo.web.webclient.plugins.FilterPluginCsp;
 /**
  * 
  * @author Morten Haraldsen
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration("classpath:/my-servlet-context.xml")
 public class RequestMultiMatcherTest
 {
-	@Autowired
-	private WebApplicationContext wac;
-
 	private SimpleController controller = new SimpleController();
-	
 	private MockMvc mockMvc;
     
     @Test
